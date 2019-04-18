269a295e22bca5ee27f15f037045043bb31493c9
Edgard Levy <edgardlevy.apple@gmail.com>
2019-04-18 17:54:49 -0300

    static server created

---
+++ public/js/config/configDev.js
@@ -0,0 +1,63 @@
+// ATENÇÃO! Não edite esse arquivo se você não souber o que está fazendo!
+
+// Configurações para conectar-se ao servidor.
+hostnameDoServidor = '192.168.1.4' // NÃO COLOCAR A "/" NO FINAL!
+portaDoServidor = 810 // NÃO COLOCAR OS ":" NO COMEÇO!
+enderecoDoCliente = 'http://192.168.1.4:8888/processamento?usuarioId=XXX'
+
+// Adicione aqui todos os motivos de liberação
+// que devem ser exibidos na hora de liberar manualmente.
+motivosDeLiberacao = ['Pagamento em atraso', 'Aula Experimental']
+
+// Configuração dos botões na barra inferior.
+// Para adicionar um novo botão, basta adicionar um novo objeto.
+configuracoesStatusBar = [
+  {
+    titulo: "Liberar Catrata 1",
+    endpoint: "http://192.168.1.4:8080/metodos/liberaCatraca?sentidoHorarioLiberado=true&sentidoAntiHorarioLiberado=true&liberacaoTempo=10000&texto=LIBERADO",
+    filtro: 'catraca1'
+  },
+  {
+    titulo: "Autorizar Catrata 47",
+    endpoint: "http://192.168.1.4:8080/metodos/liberaCatraca?sentidoHorarioLiberado=true&sentidoAntiHorarioLiberado=true&liberacaoTempo=10000&texto=LIBERADO",
+    filtro: 'catraca1'
+  },
+  {
+    titulo: "Liberar Catrata 131",
+    endpoint: "http://192.168.1.4:8080/metodos/liberaCatraca?sentidoHorarioLiberado=true&sentidoAntiHorarioLiberado=true&liberacaoTempo=10000&texto=LIBERADO",
+    filtro: 'catraca1'
+  },
+  {
+    titulo: "Liberar Catraca 2",
+    endpoint: "http://192.168.1.4:8080/metodos/liberaCatraca?sentidoHorarioLiberado=true&sentidoAntiHorarioLiberado=true&liberacaoTempo=10000&texto=LIBERADO",
+    filtro: 'catraca1'
+  }
+]
+
+// Configurações individuais de cada tela.
+// Para criar uma nova tela, basta adicionar um novo objeto.
+// Quanto maior o número de telas, recomenda-se que menor seja a fonte.
+config = [
+  {
+    titulo: "Catraca Principal",
+    corDaBorda: 'yellow',
+    fonte: 'Arial',
+    tamanhoDaFonte: 20,
+    corDoFundo: 'black',
+    corSucesso: 'green',
+    corFracasso: 'red',
+    filtro: ['catraca1'],
+    endpoint_lib_identificada: 'http://192.168.1.4:8080/metodos/liberaCatraca?sentidoHorarioLiberado=true&sentidoAntiHorarioLiberado=true&liberacaoTempo=10000&texto=LIBERADO'
+  },
+  {
+    titulo: "Catraca Emergencial",
+    corDaBorda: 'yellow',
+    fonte: 'Arial',
+    tamanhoDaFonte: 20,
+    corDoFundo: 'black',
+    corSucesso: 'green',
+    corFracasso: 'red',
+    filtro: ['catraca1', 'catraca2'],
+    endpoint_lib_identificada: 'http://192.168.1.4:8080/metodos/liberaCatraca?sentidoHorarioLiberado=true&sentidoAntiHorarioLiberado=true&liberacaoTempo=10000&texto=LIBERADO'
+  },
+]
---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var nStatic = require('node-static');
+
+var fileServer = new nStatic.Server('./public');
+
+http.createServer(function (req, res) {
+    
+    fileServer.serve(req, res);
+
+}).listen(5000,function(err){
+    if (err)
+      console.log(err)
+    else  
+      console.log('app runing on port 5000');
+});
